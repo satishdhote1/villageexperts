3295f210d757cc4fe8fd12895e4283c7af9ab723
Altanai <tara181989@gmail.com>
2016-05-02 15:41:32 +0530

    chnaged db credentails

--- config.php
+++ config.php
@@ -1,7 +1,8 @@
 <?php
 $mysql_hostname = "localhost";
 $mysql_user = "root";
-$mysql_password = "Movingpixel@2014";
+/*$mysql_password = "Movingpixel@2014";*/
+$mysql_password = "mysqlroot";
 $mysql_database = "vexpartdb";
 $bd = mysqli_connect($mysql_hostname, $mysql_user, $mysql_password, $mysql_database) or die("Could not connect database");
 mysqli_select_db($bd, $mysql_database) or die("Could not select database");
