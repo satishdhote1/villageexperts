87abb3990932ffb434a55c160e6746bd69134f45
satishdhote1 <satishdhote@gmail.com>
2016-06-28 21:40:23 +0530

    email configuration change

--- config/connection.php
+++ config/connection.php
@@ -18,11 +18,15 @@ class connections
 	private $emailPassword ='dassamtest253';
 	private $host= 'smtp.gmail.com';*/
 
-	private $emailUsername ='AKIAJRGMUMZO76PAE7VQ';
+	/*private $emailUsername ='AKIAJRGMUMZO76PAE7VQ';
 	private $emailPassword ='AvwImGIsRCL1uE0NVdqErPxo3P8FOhg0RUIWoRoMdvx6';
 	private $host= 'ses-smtp-user.20160627-205547';
 	private $from ="villageexpert.info@gmail.com";
-
+	*/
+	private $emailUsername ='AKIAJDE2TN3CRH5SMSVQ';
+	private $emailPassword ='Ah9ElPU5hKXb/28bQAdkeiTT1+YZ8JZMQwElIGt8ygni';
+	private $host= 'email-smtp.us-west-2.amazonaws.com';
+	private $from ="villageexpert.info@gmail.com
 	public function connect()
 	{	
 		$con=mysqli_connect($this->servername,$this->uname,$this->password,$this->dbname);
