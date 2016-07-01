18bd0f3f6ee7c9603548af7b2f9169ab2eee0949
satishdhote1 <satishdhote@gmail.com>
2016-07-01 22:59:48 +0530

    send mail.php

--- phpSendMail.php
+++ phpSendMail.php
@@ -1,6 +1,6 @@
 <?php
 
-require("../Documents/Unnamed Site 2/phpMailer/class.phpmailer.php");
+require("phpMailer/class.phpmailer.php");
 
 class phpSendMail
 {
@@ -9,16 +9,49 @@ private $emailUsername ="AKIAJDE2TN3CRH5SMSVQ";
 private $emailPassword ="Ah9ElPU5hKXb/28bQAdkeiTT1+YZ8JZMQwElIGt8ygni";
 private $host= "email-smtp.us-west-2.amazonaws.com";
 private $from ="dassamtest2@gmail.com";//"villageexpert.info@gmail.com";
-
+private $port = "587"; // 8025, 587 and 25 can also be used. Use Port 465 for SSL
+private $from_name ="Village Expert";
 public function geEmailConfig()
 {
 		$emailData['uname'] = $this->emailUsername;
 		$emailData['pwd'] = $this->emailPassword;
 		$emailData['host'] = $this->host;
 		$emailData['from'] = $this->from;
+		$emailData['port'] = $this->port;
+		$emailData['from_name'] = $this->from_name;
 		return $emailData; 
 		
 }
+public function sendMail($email,$memberName,$subject,$body)
+{
+	$mail = new PHPMailer();
+	$mail->SMTPDebug = 1;
+        $mail->IsSMTP();
+        $mail->Mailer = "smtp";
+        $mail->Host = $this->host;//"smtp.gmail.com";
+        $mail->Port = $this->port;.
+        $mail->SMTPAuth = true;
+        $mail->SMTPSecure = 'tls';
+    	$mail->Username = $this->emailUsername;
+	$mail->Password = $this->emailPassword;
+   	$mail->From     = $this->from;
+   	$mail->FromName = $this->from_name;
+   	$mail->AddAddress($email, $memberName);
+  	// $mail->AddReplyTo("Your Reply-to Address", "Sender's Name");
+   	$mail->Subject = $subject;
+   	$mail->Body    = $body;
+   	$mail->WordWrap = 50;  
+   	$mail->IsHTML(true);
+   	if(!$mail->Send())
+   	{
+   		return false;
+   	}
+   	else
+   	{
+   		return true;	
+   	}
+   	
 }
 
+}
 ?>
