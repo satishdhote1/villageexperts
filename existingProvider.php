65778aa60ddcbf196e2697687b8aaa7b50f8a274
satishdhote1 <satishdhote@gmail.com>
2016-07-05 08:57:10 +0530

    existing Provider

---
+++ existingProvider.php
@@ -0,0 +1,210 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="description" content="">
+<meta name="author" content="">
+<title>village expart</title>
+
+<!-- Bootstrap Core CSS -->
+<link rel="stylesheet" href="css/font-awesome.min.css">
+<link href="css/bootstrap.min.css" rel="stylesheet">
+<link href="css/mainstyle.css" rel="stylesheet">
+</head>
+<style>
+.over-lap {
+	display: block !important
+}
+</style>
+<body class="bodybg">
+<div class="container-fluid header-part">
+  <div class="row">
+    <div class="col-md-12 text-center">
+      <div class="logo"> <img src="images/logo.png" alt="logo" > </div>
+      <div class="over-lap">
+        <div class="profile pull-left"> <img src="images/img-3.jpg" class="img-responsive"> </div>
+        <div class="pull-right">
+          <p class="loginname">SUBHASIS NASKAR</p>
+          <button class="btn btn-info bg-blue">Logout</button>
+        </div>
+        <div class="clearfix"></div>
+      </div>
+    </div>
+  </div>
+</div>
+
+
+
+<div class="container search-background margin-top60">
+  <div class="row marginTOP">
+    <div class="col-md-12 text-center">
+      <h1 class="result-text">Result</h1>
+      <div class="background-blue">
+      <table class="table table-hover">
+  <thead>
+    <tr class="hder-modify-table">
+      <th width="14.2%">Image</th>
+      <th width="14.2%">Name</th>
+      <th width="14.2%">Expertish</th>
+      <th width="14.2%">Specialization</th>
+      <th width="14.2%">Experience</th>
+       <th width="14.2%">Rate/Hr</th>
+        <th width="14.2%">Language</th>
+        <th>&nbsp;</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td align="center"><div class="result-profile"><img src="images/img-3.jpg"></div></td>
+      <td>Ram Sing</td>
+      <td>Madical</td>
+      <td>Cardiac</td>
+      <td>5</td>
+       <td>$40</td>
+       <td>English</td>
+       <td><p class="connect-btn btn">Connect</p></td>
+     
+       
+    </tr>
+    <tr>
+      <td align="center"><div class="result-profile"><img src="images/img-3.jpg"></div></td>
+      <td>Ram Sing</td>
+      <td>Madical</td>
+      <td>Cardiac</td>
+      <td>5</td>
+       <td>$40</td>
+       <td>English</td>
+       <td><p class="connect-btn btn">Connect</p></td>
+     
+       
+    </tr>
+   <tr>
+      <td align="center"><div class="result-profile"><img src="images/img-3.jpg"></div></td>
+      <td>Ram Sing</td>
+      <td>Madical</td>
+      <td>Cardiac</td>
+      <td>5</td>
+       <td>$40</td>
+       <td>English</td>
+       <td><p class="connect-btn btn">Connect</p></td>
+     
+       
+    </tr>
+    <tr>
+      <td align="center"><div class="result-profile"><img src="images/img-3.jpg"></div></td>
+      <td>Ram Sing</td>
+      <td>Madical</td>
+      <td>Cardiac</td>
+      <td>5</td>
+       <td>$40</td>
+       <td>English</td>
+       <td><p class="connect-btn btn">Connect</p></td>
+     
+       
+    </tr>
+    <tr>
+      <td align="center"><div class="result-profile"><img src="images/img-3.jpg"></div></td>
+      <td>Ram Sing</td>
+      <td>Madical</td>
+      <td>Cardiac</td>
+      <td>5</td>
+       <td>$40</td>
+       <td>English</td>
+       <td><p class="connect-btn btn">Connect</p></td>
+     
+       
+    </tr>
+    <tr>
+      <td align="center"><div class="result-profile"><img src="images/img-3.jpg"></div></td>
+      <td>Ram Sing</td>
+      <td>Madical</td>
+      <td>Cardiac</td>
+      <td>5</td>
+       <td>$40</td>
+       <td>English</td>
+       <td><p class="connect-btn btn">Connect</p></td>
+     
+       
+    </tr>
+    <tr>
+      <td align="center"><div class="result-profile"><img src="images/img-3.jpg"></div></td>
+      <td>Ram Sing</td>
+      <td>Madical</td>
+      <td>Cardiac</td>
+      <td>5</td>
+       <td>$40</td>
+       <td>English</td>
+       <td><p class="connect-btn btn">Connect</p></td>
+     
+       
+    </tr>
+    <tr>
+      <td align="center"><div class="result-profile"><img src="images/img-3.jpg"></div></td>
+      <td>Ram Sing</td>
+      <td>Madical</td>
+      <td>Cardiac</td>
+      <td>5</td>
+       <td>$40</td>
+       <td>English</td>
+       <td><p class="connect-btn btn">Connect</p></td>
+     
+       
+    </tr>
+    <tr>
+      <td align="center"><div class="result-profile"><img src="images/img-3.jpg"></div></td>
+      <td>Ram Sing</td>
+      <td>Madical</td>
+      <td>Cardiac</td>
+      <td>5</td>
+       <td>$40</td>
+       <td>English</td>
+       <td><p class="connect-btn btn">Connect</p></td>
+     
+       
+    </tr>
+    <tr>
+      <td align="center"><div class="result-profile"><img src="images/img-3.jpg"></div></td>
+      <td>Ram Sing</td>
+      <td>Madical</td>
+      <td>Cardiac</td>
+      <td>5</td>
+       <td>$40</td>
+       <td>English</td>
+       <td><p class="connect-btn btn">Connect</p></td>
+     
+       
+    </tr>
+    <tr>
+      <td align="center"><div class="result-profile"><img src="images/img-3.jpg"></div></td>
+      <td>Ram Sing</td>
+      <td>Madical</td>
+      <td>Cardiac</td>
+      <td>5</td>
+       <td>$40</td>
+       <td>English</td>
+       <td><p class="connect-btn btn">Connect</p></td>
+     
+       
+    </tr>
+     </tbody>
+</table>
+       <div style="margin:20px 0px"><a class="navmenu btn-connect" href="#"><span><i class="fa fa-link"></i></span>Connect</a></div>
+      </div>
+    </div>
+    </div>
+  
+  
+  
+  
+</div>
+
+
+<!-- jQuery Version 1.11.1 --> 
+<script src="js/jquery.js"></script> 
+
+<!-- Bootstrap Core JavaScript --> 
+<script src="js/bootstrap.min.js"></script>
+</body>
+</html>
