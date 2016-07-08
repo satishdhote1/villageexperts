c63454882eca1b04d9c7d14462cc55511ea0c8f8
satishdhote1 <satishdhote@gmail.com>
2016-07-08 08:13:56 +0530

    existing provider.php

--- existingProvider.php
+++ existingProvider.php
@@ -24,9 +24,10 @@ else if($_SESSION['logged_role_code']=='GM')
 else
 $imagePath = "/";
 
-/*
+echo $user_id;
+
 //Get specialization Data
-$sql="select * from 	sp_specialisation order by specialisation";
+$sql="select specialisation_id,specialisation	 from 	sp_specialisation order by specialisation";
 			$tableResult = mysqli_query($conn, $sql);
 			//print_r($tableResult);
 
@@ -37,7 +38,75 @@ $sql="select * from 	sp_specialisation order by specialisation";
 				$specialData[] = $row;
 				}
 			}
-	*/
+
+//Get Sub specialization Data
+$sql2="select sub_specialisation_id,sub_specialisation from 	sp_sub_specialisation order by sub_specialisation";
+			$tableResult2 = mysqli_query($conn, $sql2);
+			//print_r($tableResult);
+
+				$subspecialData = array();
+			if (mysqli_num_rows($tableResult2) > 0)  
+			{
+				while($row = mysqli_fetch_assoc($tableResult2)) {
+				$subspecialData[] = $row;
+				}
+			}
+			//print_r($subspecialData);die();
+			//Get Sub Degree Data
+$sql3="select EducationID,Education from 	education ORDER BY EducationID";
+			$tableResult3 = mysqli_query($conn, $sql3);
+			//print_r($tableResult);
+
+				$education = array();
+			if (mysqli_num_rows($tableResult3) > 0)  
+			{
+				while($row = mysqli_fetch_assoc($tableResult3)) {
+				$education[] = $row;
+				}
+			}
+
+//Get Sub specialization Data
+$sql4="select ExperienceID,Experience from 	experience ORDER BY ExperienceID";
+			$tableResult4 = mysqli_query($conn, $sql4);
+			//print_r($tableResult);
+
+				$experience = array();
+			if (mysqli_num_rows($tableResult4) > 0)  
+			{
+				while($row = mysqli_fetch_assoc($tableResult4)) {
+				$experience[] = $row;
+				}
+			}
+			
+			//Get Sub Language Data
+$sql5="select language_id,languages from 	sp_language ORDER BY languages";
+			$tableResult5 = mysqli_query($conn, $sql5);
+			//print_r($tableResult);
+
+				$language = array();
+			if (mysqli_num_rows($tableResult5) > 0)  
+			{
+				while($row = mysqli_fetch_assoc($tableResult5)) {
+				$language[] = $row;
+				}
+			}
+			
+			
+//Get specialization Data
+$sql="select user.start_date_time,user.end_date_time,user.ammount, sp.sp_name,sp.sp_image,sp.sp_specialisation_id,sp.sp_sub_specialisation_id,sp.sp_year_of_experience,sp.sp_rate_type3,sp.degree,sp.sp_language_id from  connect user , service_provider sp where user.sr_id = ".$user_id." and user.sp_id in (select sp_id from service_provider) group by user.connect_id order by user.connect_id desc";
+			$tableResult = mysqli_query($conn, $sql);
+			//print_r($tableResult);
+
+				$connectData = array();
+			if (mysqli_num_rows($tableResult) > 0)  
+			{
+				while($row = mysqli_fetch_assoc($tableResult)) {
+				$connectData[] = $row;
+				}
+			}
+			//print_r($connectData);die();
+			
+	
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -105,152 +174,113 @@ $sql="select * from 	sp_specialisation order by specialisation";
       <table class="table table-hover">
   <thead>
     <tr class="hder-modify-table">
-      <th width="14.2%">Image</th>
-      <th width="14.2%">Name</th>
-      <th width="14.2%">Expertish</th>
-      <th width="14.2%">Specialization</th>
-      <th width="14.2%">Experience</th>
-       <th width="14.2%">Rate/Hr</th>
-        <th width="14.2%">Language</th>
+      <th width="9.09%"></th>
+      <th width="9.09%">Name</th>
+      <th width="9.09%">Expertise</th>
+      <th width="9.09%">Specialization</th>
+      <th width="9.09%">Education</th>
+      <th width="9.09%">Experience</th>
+       <th width="9.09%">Rate/Hr</th>
+        <th width="9.09%">Language</th>
+        <th width="9.09%">Start Time</th>
+        <th width="9.09%">End Time</th>
+        <th width="9.09%">Ammount</th>
         <th>&nbsp;</th>
     </tr>
   </thead>
   <tbody>
+<?php
+	foreach($connectData as $connectDatas)
+	{
+?>
     <tr>
-      <td align="center"><div class="result-profile"><img src="images/img-3.jpg"></div></td>
-      <td>Ram Sing</td>
-      <td>Madical</td>
-      <td>Cardiac</td>
-      <td>5</td>
-       <td>$40</td>
-       <td>English</td>
+      <td align="center"><div class="result-profile"><img src="images/SP_Photos/<?php echo $connectDatas['sp_image']; ?>"></div></td>
+      <td><?php echo $connectDatas['sp_name']; ?></td>
+      <?php
+	  
+	  foreach($specialData as $specialDatas)
+	  {
+		 // print_r($specialDatas);
+		  //echo $specialDatas['sp_specialisation_id']."--".$connectDatas['sp_specialisation_id']."<br>";
+		  if($specialDatas['specialisation_id']==$connectDatas['sp_specialisation_id'])
+		  {
+			  ?>
+              <td><?php echo !empty($specialDatas['specialisation'])?$specialDatas['specialisation']:'-'; ?></td>
+              <?php
+			  break;
+		  }
+	  }
+	 
+	  foreach($subspecialData as $subspecialDatas)
+	  {
+		  if($subspecialDatas['sub_specialisation_id']==$connectDatas['sp_sub_specialisation_id'])
+		  {
+			  ?>
+              <td><?php echo !empty($subspecialDatas['sub_specialisation'])?$subspecialDatas['sub_specialisation']:'-'; ?></td>
+              <?php
+			  break;
+		  }
+	  }
+	  
+	   foreach($education as $educationDatas)
+	  {
+		 // print_r($connectDatas);
+		 //echo $educationDatas['EducationID']."--".$connectDatas['degree']."<br>";
+		  if($educationDatas['EducationID'] == $connectDatas['degree'])
+		  {
+			  ?>
+              <td><?php echo !empty($educationDatas['Education'])?$educationDatas['Education']:'-'; ?></td>
+              <?php
+			  break;
+		  }
+	  }
+
+
+	 foreach($experience as $experienceDatas)
+	  {
+		 // print_r($connectDatas);
+		 //echo $educationDatas['EducationID']."--".$connectDatas['degree']."<br>";
+		  if($experienceDatas['ExperienceID'] == $connectDatas['sp_year_of_experience'])
+		  {
+			  ?>
+              <td><?php echo !empty($experienceDatas['Experience'])?$experienceDatas['Experience']:'-'; ?></td>
+              <?php
+			  break;
+		  }
+	  }
+	  
+	  
+	  ?>
+      <td><?php echo !empty($connectDatas['sp_rate_type3'])?$connectDatas['sp_rate_type3']:'-'; ?></td>
+      <?php
+	  
+	  foreach($language as $languageDatas)
+	  {
+		 // print_r($connectDatas);
+		 //echo $educationDatas['EducationID']."--".$connectDatas['degree']."<br>";
+		  if($languageDatas['language_id'] == $connectDatas['sp_language_id'])
+		  {
+			  ?>
+              <td><?php echo !empty($languageDatas['languages'])?$languageDatas['languages']:'-'; ?></td>
+              <?php
+			  break;
+		  }
+	  }
+	  ?>
+       <td><?php echo !empty($connectDatas['start_date_time'])?$connectDatas['start_date_time']:'-'; ?></td>
+       <td><?php echo !empty($connectDatas['end_date_time'])?$connectDatas['end_date_time']:'-'; ?></td>
+       <td><?php echo !empty($connectDatas['ammount'])?$connectDatas['ammount']:'-'; ?></td>
        <td><p class="connect-btn btn">Connect</p></td>
      
        
     </tr>
-    <tr>
-      <td align="center"><div class="result-profile"><img src="images/img-3.jpg"></div></td>
-      <td>Ram Sing</td>
-      <td>Madical</td>
-      <td>Cardiac</td>
-      <td>5</td>
-       <td>$40</td>
-       <td>English</td>
-       <td><p class="connect-btn btn">Connect</p></td>
-     
-       
-    </tr>
-   <tr>
-      <td align="center"><div class="result-profile"><img src="images/img-3.jpg"></div></td>
-      <td>Ram Sing</td>
-      <td>Madical</td>
-      <td>Cardiac</td>
-      <td>5</td>
-       <td>$40</td>
-       <td>English</td>
-       <td><p class="connect-btn btn">Connect</p></td>
-     
-       
-    </tr>
-    <tr>
-      <td align="center"><div class="result-profile"><img src="images/img-3.jpg"></div></td>
-      <td>Ram Sing</td>
-      <td>Madical</td>
-      <td>Cardiac</td>
-      <td>5</td>
-       <td>$40</td>
-       <td>English</td>
-       <td><p class="connect-btn btn">Connect</p></td>
-     
-       
-    </tr>
-    <tr>
-      <td align="center"><div class="result-profile"><img src="images/img-3.jpg"></div></td>
-      <td>Ram Sing</td>
-      <td>Madical</td>
-      <td>Cardiac</td>
-      <td>5</td>
-       <td>$40</td>
-       <td>English</td>
-       <td><p class="connect-btn btn">Connect</p></td>
-     
-       
-    </tr>
-    <tr>
-      <td align="center"><div class="result-profile"><img src="images/img-3.jpg"></div></td>
-      <td>Ram Sing</td>
-      <td>Madical</td>
-      <td>Cardiac</td>
-      <td>5</td>
-       <td>$40</td>
-       <td>English</td>
-       <td><p class="connect-btn btn">Connect</p></td>
-     
-       
-    </tr>
-    <tr>
-      <td align="center"><div class="result-profile"><img src="images/img-3.jpg"></div></td>
-      <td>Ram Sing</td>
-      <td>Madical</td>
-      <td>Cardiac</td>
-      <td>5</td>
-       <td>$40</td>
-       <td>English</td>
-       <td><p class="connect-btn btn">Connect</p></td>
-     
-       
-    </tr>
-    <tr>
-      <td align="center"><div class="result-profile"><img src="images/img-3.jpg"></div></td>
-      <td>Ram Sing</td>
-      <td>Madical</td>
-      <td>Cardiac</td>
-      <td>5</td>
-       <td>$40</td>
-       <td>English</td>
-       <td><p class="connect-btn btn">Connect</p></td>
-     
-       
-    </tr>
-    <tr>
-      <td align="center"><div class="result-profile"><img src="images/img-3.jpg"></div></td>
-      <td>Ram Sing</td>
-      <td>Madical</td>
-      <td>Cardiac</td>
-      <td>5</td>
-       <td>$40</td>
-       <td>English</td>
-       <td><p class="connect-btn btn">Connect</p></td>
-     
-       
-    </tr>
-    <tr>
-      <td align="center"><div class="result-profile"><img src="images/img-3.jpg"></div></td>
-      <td>Ram Sing</td>
-      <td>Madical</td>
-      <td>Cardiac</td>
-      <td>5</td>
-       <td>$40</td>
-       <td>English</td>
-       <td><p class="connect-btn btn">Connect</p></td>
-     
-       
-    </tr>
-    <tr>
-      <td align="center"><div class="result-profile"><img src="images/img-3.jpg"></div></td>
-      <td>Ram Sing</td>
-      <td>Madical</td>
-      <td>Cardiac</td>
-      <td>5</td>
-       <td>$40</td>
-       <td>English</td>
-       <td><p class="connect-btn btn">Connect</p></td>
-     
-       
-    </tr>
+  <?php
+  
+	  }
+  ?>  
      </tbody>
 </table>
-       <div style="margin:20px 0px"><a class="navmenu btn-connect" href="#"><span><i class="fa fa-link"></i></span>Connect</a></div>
+       <!--<div style="margin:20px 0px"><a class="navmenu btn-connect" href="#"><span><i class="fa fa-link"></i></span>Connect</a></div>-->
       </div>
     </div>
     </div>
