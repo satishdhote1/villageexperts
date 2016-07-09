29fcc8e67df4c8756fe74cd1c7b4cb74716bb757
satishdhote1 <satishdhote@gmail.com>
2016-07-09 12:38:58 +0530

    Update phpSendMail.php

--- phpSendMail.php
+++ phpSendMail.php
@@ -4,10 +4,15 @@ require("phpMailer/class.phpmailer.php");
 
 class phpSendMail
 {
-
+/*
 private $emailUsername ="AKIAJDE2TN3CRH5SMSVQ";
 private $emailPassword ="Ah9ElPU5hKXb/28bQAdkeiTT1+YZ8JZMQwElIGt8ygni";
 private $host= "email-smtp.us-west-2.amazonaws.com";
+*/
+
+private $emailUsername ="AKIAIVWQDQ5MXUANO4ZA";
+private $emailPassword ="AoBHQwH1NZ+wZIPeSOGa5sOoF1wmhivD2RlX8thYdlyl";
+private $host= "smtp.us-west-2.amazonaws.com";
 private $from ="dassamtest2@gmail.com";//"villageexpert.info@gmail.com";
 private $port = "587"; // 8025, 587 and 25 can also be used. Use Port 465 for SSL
 private $from_name ="Village Expert";
@@ -36,7 +41,7 @@ public function sendMail($email,$memberName,$subject,$body)
 	$mail->Password = $this->emailPassword;
    	$mail->From     = $this->from;
    	$mail->FromName = $this->from_name;
-   	$mail->AddAddress($email, $memberName);
+   	$mail->AddAddress("satishdhote@gmail.com", $memberName);
   	// $mail->AddReplyTo("Your Reply-to Address", "Sender's Name");
    	$mail->Subject = $subject;
    	$mail->Body    = $body;
