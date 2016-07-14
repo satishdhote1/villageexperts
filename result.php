19a71cea39610fd97d2038d5b35acacc6fadb98c
satishdhote1 <satishdhote@gmail.com>
2016-07-14 08:02:10 +0530

    Update result.php

--- result.php
+++ result.php
@@ -615,7 +615,7 @@ else{
 <script src="js/bootstrap.min.js"></script>
 
 <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
-
+<script src="js/connectMe.js"></script>
   <script src="js/jquery.flipster.js"></script>
 
 <script>
