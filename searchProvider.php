808d8099b4fe9013869b1c5b7d072faffeef4c24
satishdhote1 <satishdhote@gmail.com>
2016-07-22 01:17:25 +0530

    Update searchProvider.php

--- searchProvider.php
+++ searchProvider.php
@@ -124,7 +124,7 @@ $sql5="select * from 	sp_language ORDER BY languages";
     z-index: 100000;}
 	.header-part{position:fixed;z-index:100000;background:#E9E9EA;}
 	.block-4-section{padding-top:260px;}
-	.btn-search-2{background:#ff8003;border:0px;padding:7px 45px;color:#fff;transition:all ease-in-out 0.2s 0s;margin-top:20px;}
+	.btn-search-2{background:#ff8003;border:0px;padding:7px 45px;color:#fff;transition:all ease-in-out 0.2s 0s;margin-top:28%;/*20px;*/}
 	.btn-search-2:hover{background:#697bff;color:#eaecff;}
 	.radius0{border-radius:0px;}
 	.display-list > li {
