afbea832b24026e9a8bf85fca0a85471655aeb95
satishdhote1 <satishdhote@gmail.com>
2016-07-23 00:09:32 +0530

    Update searchProvider.php

--- searchProvider.php
+++ searchProvider.php
@@ -460,6 +460,7 @@ input[type="radio"] + label span, input[type="radio"]:checked + label span {
   
   <div class="box-2">
     <div class="col-md-3 col-xs-5 text-center">
+    	<span id="js-output" style="background:gold;font-size:25px;"></span>
     <div class="back-ground">
     <span id="js-output"></span>
     <p class="title-name-2">rate per hour</p>
