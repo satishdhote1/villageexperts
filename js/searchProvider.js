f207809aaa224e0b5fe0c728525c74191315759e
satishdhote1 <satishdhote@gmail.com>
2016-07-28 22:37:08 +0530

    Update searchProvider.js

--- js/searchProvider.js
+++ js/searchProvider.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-	$(document).on("click",".btn-search-3",function(){
+	$(document).on("click",".btn-search-3-searchProvider ",function(){
 		
 		$("#search").submit();
 	});
