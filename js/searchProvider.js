d99c260ce31ab4befc72824b6f0dd6f3ca7c4c9f
satishdhote1 <satishdhote@gmail.com>
2016-07-28 06:42:07 +0530

    Update searchProvider.js

--- js/searchProvider.js
+++ js/searchProvider.js
@@ -135,7 +135,7 @@ $("input:checkbox").on('click', function() {
                    });
 		//	alert( $(".removeExp").offset().top);
                       $('.setSubSpecialData').html(resultData);
-                      $('html,body').animate({scrollTop: $(".specialData").offset().top}, 900,'swing');
+                      $('html,body').animate({scrollTop: parseInt($(".specialData").offset().top,10)}, 500,'swing');
 					 $(".removeExp").each(function(index, element) {
 					 
 					  if($(this).hasClass("over"))
