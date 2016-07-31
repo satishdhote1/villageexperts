d27d09800d1fdfc2aeb1d025a03372ef356a9f43
satishdhote1 <satishdhote@gmail.com>
2016-07-31 23:19:48 +0530

    Update add-new-member.php

--- add-new-member.php
+++ add-new-member.php
@@ -129,6 +129,7 @@ $getgroupName = isset($_REQUEST['groupName'])? $_REQUEST['groupName'] : '';
 <!-- Bootstrap Core JavaScript --> 
 <script src="js/bootstrap.min.js"></script>
 <script src="js/connectMe.js"></script> 
+<script src="js/formSubmission.js"></script> 
 <!--<script src="js/instantImageShow.js"></script>-->
 
 </body>
