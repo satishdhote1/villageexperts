26a8125431d2703c5b17e33ba92dd0ac537abcc5
altanai <tara181989@gmail.com>
2017-03-14 05:01:09 +0000

    some images

--- info.php
+++ info.php
@@ -1,5 +1,11 @@
 <?php
 echo "this is latest change";
 echo "http://" . $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI']; 
-echo "<br>".dirname(__FILE__)
+echo "<br>".dirname(__FILE__);
+
+
+echo phpinfo();
+
+
+
 ?>
