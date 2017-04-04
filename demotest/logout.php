8bc8b93c1908ba36d237ba6e92888bcc153c6af0
Altanai <tara181989@gmail.com>
2017-04-04 12:19:41 +0530

    remove demotest

--- demotest/logout.php
+++
@@ -1,9 +0,0 @@
-<?php 
-session_start();
-session_destroy();
-session_unset();
-			$_SESSION['SESS_ID'] = '';
-			$_SESSION['SESS_NAME'] = '';
-			$_SESSION['SESS_EMAIL'] = '';
-			header('location:index.php');
-?>
