9970bc56a2872bda0cf0137338196720851ff6dd
Altanai <tara181989@gmail.com>
2017-04-10 22:52:07 +0530

    header footer

--- friends-family.php
+++ friends-family.php
@@ -241,45 +241,8 @@ if (mysqli_num_rows($tableResult4) > 0)  {
 
 <body>
 
-    <!-- Start your project here-->
-    <!--Navbar-->
 
-    <div class="login-profile">
-        <a href="/" data-toggle="tooltip" data-placement="bottom" title="Back"><img src="images/Left.png" class="topBtn" style="cursor:pointer;"/></a>                
-        <a href="logout.php" data-toggle="tooltip" data-placement="bottom" title="Log out!"><img src="images/logout.png" class="topBtn" style="cursor:pointer;"/></a>
-    </div>
-
-    <nav class="navbar navbar-dark" style="background:#ccccff;">
-
-<!-- 
-//subhasis
-<div class="over-lap">
-      <div class="profile pull-right"> <img src="<?php echo (!empty($imagePath))?$imagePath:"images/placeholder/maleDummy.png"; ?>" class="img-responsive"> </div>
-      <div class="pull-right">
-      <p class="loginname">
-      <?php
-      //echo "Welcome ".$userNme."!";  
-      ?>
-      </p>
-      <div class=""><a href="logout.php" class="btn btn-info bg-blue logout">Logout</a></div>
-      </div>
-      <div class="clearfix"></div>
-      </div> -->
-
-        <div class="container">
-            <!--Collapse content-->
-            <div class="logo-modify">
-                <!--Navbar Brand-->
-                <a class="navbar-brand" href="#" target="_blank">
-                    <img src="img/normal/logo.png">
-                    <p class="brand-text">VILLAGE EXPERTS</p></a>
-                <label class="modify-badge-2" style="margin-left:-46px;font-size:23px;padding:0px;color:#495775">My Home Page</label>
-             </div>   
-        </div>
-    </nav>
-
-    <!--/.Navbar-->
-    <!--Mask-->
+    <?php include 'header.php';?>
 
    <section class="friend-family">
 
@@ -1129,6 +1092,9 @@ function isEmail(email) {
 
 </script>
 
+
+<?php include 'footer.php';?>
+
 </body>
 
 </html>
@@ -1142,3 +1108,4 @@ else
     header("location:well-come.php?passStr=$passStr&passImg=$passImg&redirect=index");
 }
 ?>
+
