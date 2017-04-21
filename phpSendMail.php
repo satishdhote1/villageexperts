3c24c8a1fb5109bf5624e6919b2870d146a11f85
satishdhote <satishdhote@gmail.com>
2017-04-22 00:27:31 +0530

    mail

--- phpSendMail.php
+++ phpSendMail.php
@@ -4,10 +4,15 @@ require("phpMailer/PHPMailerAutoload.php");
 class phpSendMail
 {
 
-private $emailUsername ="AKIAJ45NCQD6ODRRNXSQ";//AKIAJDE2TN3CRH5SMSVQ";
-private $emailPassword ="Alqm+rrqw+HcQIJcVNZmkkWB0zjOob+O7KpBP4tDfVCU";//Ah9ElPU5hKXb/28bQAdkeiTT1+YZ8JZMQwElIGt8ygni";
-private $host= "ses-smtp-user.20170412-120640";//"email-smtp.us-west-2.amazonaws.com";
+//private $emailUsername ="AKIAJ45NCQD6ODRRNXSQ";//AKIAJDE2TN3CRH5SMSVQ";
+//private $emailPassword ="Alqm+rrqw+HcQIJcVNZmkkWB0zjOob+O7KpBP4tDfVCU";//Ah9ElPU5hKXb/28bQAdkeiTT1+YZ8JZMQwElIGt8ygni";
+//private $host= "ses-smtp-user.20170412-120640";//"email-smtp.us-west-2.amazonaws.com";
 private $from ="villageexpert.info@gmail.com";//dassamtest2@gmail.com";//"villageexpert.info@gmail.com";
+
+private $emailUsername ="AKIAJDE2TN3CRH5SMSVQ";
+  private $emailPassword ="Ah9ElPU5hKXb/28bQAdkeiTT1+YZ8JZMQwElIGt8ygni";
+  private $host= "email-smtp.us-west-2.amazonaws.com";
+
 private $port = "465"; // 8025, 587 and 25 can also be used. Use Port 465 for SSL
 private $from_name ="Village Expert";
 private $smtpSecure = 'ssl';
