c16d555537429a2e55d2e71a171ea493ff8535b9
satishdhote <satishdhote@gmail.com>
2017-04-22 07:18:48 +0530

    mail

--- phpSendMail.php
+++ phpSendMail.php
@@ -3,23 +3,19 @@ require("phpMailer/class.phpmailer.php");
 require("phpMailer/PHPMailerAutoload.php");
 class phpSendMail
 {
-
-private $emailUsername ="AKIAJ45NCQD6ODRRNXSQ";//AKIAJDE2TN3CRH5SMSVQ";
-private $emailPassword ="Alqm+rrqw+HcQIJcVNZmkkWB0zjOob+O7KpBP4tDfVCU";//Ah9ElPU5hKXb/28bQAdkeiTT1+YZ8JZMQwElIGt8ygni";
-private $host= "ses-smtp-user.20170412-120640";//"email-smtp.us-west-2.amazonaws.com";
-private $from ="villageexpert.info@gmail.com";//dassamtest2@gmail.com";//"villageexpert.info@gmail.com";
-private $port = "25"; // 8025, 587 and 25 can also be used. Use Port 465 for SSL
-private $from_name ="Village Expert";
-private $smtpSecure = 'ssl';
-
 /*
+private $emailUsername ="AKIAJDE2TN3CRH5SMSVQ";
+private $emailPassword ="Ah9ElPU5hKXb/28bQAdkeiTT1+YZ8JZMQwElIGt8ygni";
+private $host= "email-smtp.us-west-2.amazonaws.com";
+private $from ="dassamtest2@gmail.com";//"villageexpert.info@gmail.com";
+private $port = "587"; // 8025, 587 and 25 can also be used. Use Port 465 for SSL
+*//*
 private $emailUsername ="dassamtest2@gmail.com";
 private $emailPassword ="dassamtest253";
 private $host= "smtp.gmail.com";
 private $from ="dassamtest2@gmail.com";//"villageexpert.info@gmail.com";
 private $port = "587"; // 8025, 587 and 25 can also be used. Use Port 465 for SSL
 */
-
 //kidzeeweb
 /*
 private $emailUsername ="villageexperts@kidzeeweb.com";
@@ -28,61 +24,57 @@ private $host= "mail.kidzeeweb.com";
 private $from ="villageexperts@kidzeeweb.com";//"villageexpert.info@gmail.com";
 private $port = "26"; // 8025, 587 and 25 can also be used. Use Port 465 for SSL
 */
-
 //otoman.net
-/*private $emailUsername ="villageexperts@otoman.net";
+private $emailUsername ="villageexperts@otoman.net";
 private $emailPassword ="WebRTC123";
-private $host= "otoman.net" ;//sg2plcpnl0137.prod.sin2.secureserver.net";
+private $host= "sg2plcpnl0137.prod.sin2.secureserver.net";
 private $from ="villageexperts@otoman.net";//"villageexpert.info@gmail.com";
 private $port = "465"; // 8025, 587 and 25 can also be used. Use Port 465 for SSL
 private $from_name ="Village Expert";
-private $smtpSecure = 'ssl';*/
-
+private $smtpSecure = 'ssl';
 public function geEmailConfig()
 {
-		$emailData['uname'] = $this->emailUsername;
-		$emailData['pwd'] = $this->emailPassword;
-		$emailData['host'] = $this->host;
-		$emailData['from'] = $this->from;
-		$emailData['port'] = $this->port;
-		$emailData['smtpSecure'] = $this->smtpSecure;
-		$emailData['from_name'] = $this->from_name;
-		return $emailData; 
-		
+    $emailData['uname'] = $this->emailUsername;
+    $emailData['pwd'] = $this->emailPassword;
+    $emailData['host'] = $this->host;
+    $emailData['from'] = $this->from;
+    $emailData['port'] = $this->port;
+    $emailData['smtpSecure'] = $this->smtpSecure;
+    $emailData['from_name'] = $this->from_name;
+    return $emailData; 
+    
 }
 public function sendMail($email,$memberName,$subject,$body)
 {
-  	//echo "email-$email--=name:$memberName*-----body---$body=====$this->emailUsername===$this->emailPassword===$this->host";
-  	$mail = new PHPMailer();
-  	$mail->SMTPDebug = 2;
-    $mail->IsSMTP();
-    $mail->Mailer = "smtp";
-    $mail->Host = $this->host;//"smtp.gmail.com";
-    $mail->Port = $this->port;
-    $mail->SMTPAuth = true;
-    $mail->SMTPSecure = $this->smtpSecure;
-    $mail->Username = $this->emailUsername;
-    $mail->Password = $this->emailPassword;
-    $mail->setFrom     = $this->from;
+  //echo "email-$email--=name:$memberName*-----body---$body=====$this->emailUsername===$this->emailPassword===$this->host";
+  $mail = new PHPMailer();
+  //$mail->SMTPDebug = 1;
+        $mail->IsSMTP();
+        $mail->Mailer = "smtp";
+        $mail->Host = $this->host;//"smtp.gmail.com";
+        $mail->Port = $this->port;
+        $mail->SMTPAuth = true;
+        $mail->SMTPSecure = $this->smtpSecure;
+      $mail->Username = $this->emailUsername;
+  $mail->Password = $this->emailPassword;
+    $mail->From     = $this->from;
     $mail->FromName = $this->from_name;
-    
     $mail->AddAddress($email, $memberName);
     // $mail->AddReplyTo("Your Reply-to Address", "Sender's Name");
     $mail->Subject = $subject;
     $mail->Body    = $body;
     $mail->WordWrap = 50;  
     $mail->IsHTML(true);
-
-   	if(!$mail->Send())
-   	{
-   		echo "Mailer Error: " . $mail->ErrorInfo;
-   		return false;
-   	}
-   	else
-   	{
-   		return true;	
-   	}
-   	
+    if(!$mail->Send())
+    {
+      echo "Mailer Error: " . $mail->ErrorInfo;
+      return false;
+    }
+    else
+    {
+      return true;  
+    }
+    
 }
 
 }
