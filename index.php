842740a628dc251edfc337b1db2750d30af20fbc
satishdhote <satishdhote@gmail.com>
2017-05-31 23:18:58 +0530

    my profile

--- index.php
+++ index.php
@@ -120,7 +120,7 @@ if(!empty($email) && !empty($isFriendreg) && $isFriendreg == "yes"){
             background-attachment:fixed;
         }
         p.btn.btn2.font-20.text-xs-left.waves-effect.waves-light:hover {
-		    box-shadow: 0 5px 11px 0 rgba(0,0,0,.18), 0 4px 15px 0 rgba(0,0,0,.15);
+		    box-shadow: 0 2px 5px 0 rgba(0,0,0,.16), 0 2px 10px 0 rgba(0,0,0,.12);
 		    cursor: default;
 		}
         .modi-img {
