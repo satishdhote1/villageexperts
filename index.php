d738a931d21e03a39d1040d5840480450dd6c6bf
satishdhote <satishdhote@gmail.com>
2017-06-19 07:36:41 +0530

    forget password

--- index.php
+++ index.php
@@ -321,7 +321,7 @@ if(!empty($email) && !empty($isFriendreg) && $isFriendreg == "yes"){
                                 </div>
 					
                 		<input type="hidden" id="friendLoginHidden" class="friendLoginHidden" value="friendsLogin">
-       
+                    <div><a href="#" data-target="#pwdModal" data-toggle="modal">Forgot my password</a></div>
                    		<div class="text-xs-center"> 
                        			<button class="btn log  modal__trigger friendLoginButton" id="friendLoginButton">LOGIN</button>
                      		</div>
@@ -539,6 +539,44 @@ if(!empty($email) && !empty($isFriendreg) && $isFriendreg == "yes"){
     </section>
     <!--Main container-->
     
+<!--modal-->
+<div id="pwdModal" class="modal fade" tabindex="-1" role="dialog" aria-hidden="true">
+  <div class="modal-dialog">
+  <div class="modal-content">
+      <div class="modal-header">
+          <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
+          <h1 class="text-center"></h1>
+      </div>
+      <div class="modal-body">
+          <div class="col-md-12">
+                <div class="panel panel-default">
+                    <div class="panel-body">
+                        <div class="text-center">
+                          
+                          <p>If you have forgotten your password you can reset it here.</p>
+                            <div class="panel-body">
+                                <fieldset>
+                                    <div class="form-group">
+                                        <input class="form-control input-lg" placeholder="E-mail Address" name="email" type="email">
+                                    </div>
+                                    <input class="btn btn-lg btn-primary btn-block" value="Send My Password" type="submit">
+                                </fieldset>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+      </div>
+      <div class="modal-footer">
+          <div class="col-md-12">
+          <button class="btn" data-dismiss="modal" aria-hidden="true">Cancel</button>
+      </div>  
+      </div>
+  </div>
+  </div>
+</div>
+
+
 
     <!-- SCRIPTS -->
 
